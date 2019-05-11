9547f57804f49a1a58bec87280f0daac1d1c1b27
Anastasia <kevin18121999@gmail.com>
2019-05-11 14:18:23 +0300

    add view tests

--- resources/views/tests.blade.php
+++ resources/views/tests.blade.php
@@ -8,10 +8,12 @@
 		<div>
 			<img src="{{$test->image}}">
 			<p>
-				@if (1 === 1)
+				@if ($test->id <= $lastPassed + 1)
 					<a href="{{$test->path}}">
 						{{$test->name}}
 					</a>
+				@else 
+					{{$test->name}}
 				@endif
 			</p>
 		</div>
