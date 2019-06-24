2a697f3ea0bdefaa626940f3b9a704d6dbc2a4bb
Anastasia <kevin18121999@gmail.com>
2019-06-25 00:44:25 +0300

    piy

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Error</title>
+    <title>Error</title>
 </head>
+
 <body>
-	<h1>Доступ запрещен</h1>
+    <h1>Доступ запрещен не авторизованным пользователям</h1>
+    <h3>Войдите по <a href="http://emmc-2019.herokuapp.com/login">ссылке</a></h3>
+    <h3>Если вы не авторизованы, то зарегистрируйтесь по <a href="http://emmc-2019.herokuapp.com/register">ссылке</a></h3>
 </body>
+
 </html>
